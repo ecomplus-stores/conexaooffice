bdd1bb61255452743ea8dc38416c51f7554c25e9
Andersen Cypriano <acypriano@mtsolucoes.com.br>
2023-08-31 22:30:19 -0300

    revisao 0.6

--- template/js/custom-js/mt-solucoes/productPage/cloneButtons/index.js
+++ template/js/custom-js/mt-solucoes/productPage/cloneButtons/index.js
@@ -6,9 +6,12 @@ function cloneContentFavoriteButton () {
   document.querySelector('.product__buy').appendChild(document.querySelector('.product__buy + div a'))
 }
 
-
+function cloneContentNotification () {
+  document.querySelector('.product__prices .prices__value').appendChild(document.querySelector('.offers-notification'))
+}
 
 export const cloneButtons = () => {
   cloneContentShare ();
   cloneContentFavoriteButton();
+  cloneContentNotification();
 }
