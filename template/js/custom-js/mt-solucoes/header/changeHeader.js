be40070ab905f1c6ab94877b8392295d2ff2d2e0
Andersen Cypriano <acypriano@mtsolucoes.com.br>
2023-09-06 16:39:00 -0300

    revisao 1.1

--- template/js/custom-js/mt-solucoes/header/changeHeader.js
+++ template/js/custom-js/mt-solucoes/header/changeHeader.js
@@ -4,12 +4,20 @@ import { resetStateButtonMiniMenu } from "./buttonMenuMiniMenu";
 const header = document.querySelector('header');
 const mainMenu = document.querySelector('.main-menu');
 
+// Create all products btn
+document.querySelector('.header__nav > div a').remove();
+const btnAllProducts = createElement('a', 'btn-all-products')
+btnAllProducts.textContent = "Todos os produtos";
+document.querySelector('.header__nav > div').prepend(btnAllProducts);
+btnAllProducts.addEventListener('click', (e)=> {
+  e.target.nextElementSibling.classList.toggle('show-submenu-main');
+})
+
 const setMainMenu = () => {
   header.classList.add('mini-header');
   header.classList.remove('mini-header');
   mainMenu.removeAttribute('id','show-main-menu');
   resetStateButtonMiniMenu();
-
 }
 
 const setMiniMenu = () => {
@@ -26,14 +34,10 @@ const setMiniMenu = () => {
   document.querySelector('.btn-close-mini-main-menu') ? null : document.querySelector('.main-menu').prepend(btnCloseMainMenu);
 
   // Custom button TODOS OS PRODUTOS
-  const buttonAllProducts = document.querySelector('.mini-header .header__nav :first-child > a');
-  buttonAllProducts.removeAttribute('onmouseover');
-  buttonAllProducts.removeAttribute('href');
-  buttonAllProducts.removeAttribute('onclick');
+  const buttonAllProducts = document.querySelector('.mini-header .header__nav div > div');
   buttonAllProducts.addEventListener('click', (e)=> {
     e.preventDefault();
     e.target.classList.toggle('opened-submenu')
-    e.target.nextElementSibling.classList.toggle('show-mini-submenu')
   })  
 }
 export const changeHeader = () => {
@@ -43,3 +47,7 @@ export const changeHeader = () => {
     }, 100);
   })
 }
+
+
+
+
